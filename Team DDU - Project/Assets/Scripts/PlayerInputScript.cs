affd4ccc21db7759dd9ab239e65b5e7e2a7d4ffc
Deivid Raychev <draychev95@mail.ru>
2014-11-10 16:01:56 +0200

    Movement and properties scripts
    Added some basic movement logic and 2 properties to the player

---
+++ Team DDU - Project/Assets/Scripts/PlayerInputScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputScript : MonoBehaviour {
+
+    private float movementSpeed;
+    private float rotationalSpeed;
+
+    private bool movingLeft;
+    private bool movingRight;
+    private bool movingForward;
+    private bool movingBack;
+
+	void Start () {
+        movementSpeed = GetComponent<PlayerPropertiesScript>().movementSpeed;
+        rotationalSpeed = GetComponent<PlayerPropertiesScript>().rotationalSpeed;
+        movingLeft = movingRight = movingForward = movingBack = false;
+	}
+	
+	void Update () {
+
+        //movement
+        if (Input.GetKeyDown(KeyCode.A))
+            movingLeft = true;
+        else if (Input.GetKeyDown(KeyCode.D))
+            movingRight = true;
+        if (Input.GetKeyDown(KeyCode.W))
+            movingForward = true;
+        else if (Input.GetKeyDown(KeyCode.S))
+            movingBack = true;
+
+        if (Input.GetKeyUp(KeyCode.A))
+            movingLeft = false;
+        if (Input.GetKeyUp(KeyCode.D))
+            movingRight = false;
+        if (Input.GetKeyUp(KeyCode.W))
+            movingForward = false;
+        if (Input.GetKeyUp(KeyCode.S))
+            movingBack = false;
+
+        if (movingLeft == true)
+        {
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+        else if (movingRight == true)
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+        if (movingForward == true)
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+        else if (movingBack == true)
+        {
+            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+        }
+	}
+}
---
+++ Team DDU - Project/Assets/Scripts/PlayerPropertiesScript.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPropertiesScript : MonoBehaviour {
+    public float movementSpeed = 3f;
+    public float rotationalSpeed = 3f;
+}
