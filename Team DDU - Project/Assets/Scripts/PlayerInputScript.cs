da93d615351092a5a0370388ef817385f2ff137d
Deivid Raychev <draychev95@mail.ru>
2014-11-11 12:00:02 +0200

    Character rotation
    Added character rotation based on mouse position

--- Team DDU - Project/Assets/Scripts/PlayerInputScript.cs
+++ Team DDU - Project/Assets/Scripts/PlayerInputScript.cs
@@ -18,6 +18,15 @@ public class PlayerInputScript : MonoBehaviour {
 	}
 	
 	void Update () {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0.0f;
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationalSpeed * Time.deltaTime);
+        }
 
         //movement
         if (Input.GetKeyDown(KeyCode.A))
