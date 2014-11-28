df1a0066787b896f14c7b1cece3498540f8dc0e8
Deivid Raychev <draychev95@mail.ru>
2014-11-29 00:11:41 +0200

    Added bullet script with deleting function after time

---
+++ Team DDU - Project/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+    public float timePeriod;
+    private float currentTime;
+    public float timeBeforeDestroy;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += timePeriod;
+        if (currentTime >= timeBeforeDestroy)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
